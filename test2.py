41bf33df52e7b19286aca5baec13a5f7cd409f38
starbuckssss <parksungwoo0128@gmail.com>
2024-03-13 17:15:56 +0900

    messeage

---
+++ test2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 16:45:45 2024
+
+@author: Sungwoo Park
+"""
+
+a = int(input("첫 번째 숫자를 입력하세요 :"))
+b = int(input("두 번째 숫자를 입력하세요 :"))
+
+result = a + b
+print(a, "+", b, "=", result)
+
+result = a - b
+print(a, "-", b, "=", result)
+
+result = a * b
+print(a, "*", b, "=", result)
+
+result = a / b
+print(a, "/", b, "=", result)
