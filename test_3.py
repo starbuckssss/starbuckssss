f07497d1dda439dc789097187572d86d339e5b38
starbuckssss <parksungwoo0128@gmail.com>
2024-03-20 17:12:13 +0900

    메세지

---
+++ test_3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 16:40:41 2024
+
+@author: Sungwoo Park
+"""
+
+print("100")
+print(100)
+print(50+50)
+print("50+50")
+print('%d/%d = %d' % (5,10,5/10))
+print("%04d" % 876)
+print("%5s"%"CookBook")
+print("%1.1f"%123.45)
+print("{2:d}{0:d}{1:d}".format(111,222,333))
+
+print(int('0011', 2))
+print(int('1A', 16))
+
+print(int('AAFG', 16))
+print(int('1002', 2))
+print(int('1008', 8))
+
+num=12345678
+hex_num = hex(num)
+oct_num = oct(num)
+bin_num=bin(num)
